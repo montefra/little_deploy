12297341424123e92d7eadd7c2e5dceef1965aef
montefra <franz.bergesund@gmail.com>
2016-02-16 15:53:51 +0100

    Use packit for packaging

--- setup.py
+++ setup.py
@@ -1,30 +1,9 @@
 from setuptools import setup
 
-
-entry_points = {'console_scripts': ['little_deploy = little_deploy:main']}
-
 setup(
-    # package description and version
-    name="little_deploy",
-    version='0.0.0',
-    author="Francesco Montesano",
-    author_email="franz.bergesund@gmail.com",
-    description="Copy files from one place to an other",
-    long_description=open("README.md").read(),
+    setup_requires='packit',
+    packit=True,
 
     # list of packages and data
     py_modules=['little_deploy'],
-
-    entry_points=entry_points,
-
-    classifiers=["Development Status :: 2 - Pre-Alpha",
-                 "Environment :: Console",
-                 "Intended Audience :: Developers",
-                 "License :: OSI Approved :: MIT License",
-                 "Operating System :: Unix",
-                 "Programming Language :: Python :: 3.4",
-                 "Programming Language :: Python :: 3.5",
-                 "Topic :: Software Development :: Documentation",
-                 "Topic :: Utilities",
-                 ]
 )
