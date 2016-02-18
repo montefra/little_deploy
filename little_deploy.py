d365daaa176ad2bf782bd1218a175710cfe9f597
montefra <franz.bergesund@gmail.com>
2016-02-18 15:38:49 +0100

    Begin implementing new features

--- little_deploy.py
+++ little_deploy.py
@@ -12,6 +12,10 @@ import pathlib
 import shutil
 import sys
 
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution('little-deploy').version
+
 
 def parse(argv=None):
     import argparse as ap
@@ -41,8 +45,44 @@ def parse(argv=None):
     return p.parse_args(args=argv)
 
 
+def check_type(type_):
+    """Check that ``type_`` is not one of the reserved options
+
+    Parameters
+    ----------
+    type_ : string
+        name to check
+
+    Raises
+    ------
+    ValueError
+        if the name is among the reserved ones
+    """
+    if type_ in ['pkg_name', 'overwrite_releases', 'overwrite_dev',
+                 'version_dev']:
+        raise ValueError('The name "{}" for the type is not'
+                         ' allowed'.format(type_))
+
+
+def get_version(deploy_dir, conf):
+    """Check if the deploy directory requires the version and deal with it
+    properly"""
+    has_version = (('{version}' in deploy_dir) and
+                   ('{{version}}' not in deploy_dir) and
+                   ('${version}' not in deploy_dir))
+    version = None
+
+    if has_version:
+        pkg_name = conf['pkg_name']
+        pversion = pkg_resources.get_distribution(pkg_name).parsed_version
+
+    return version
+
+
 def main(argv=None):
     args = parse(argv=argv)
+
+    check_type(args.type)
 
     conf = ConfigParser(interpolation=ExtendedInterpolation())
     if not conf.read(os.path.expanduser(args.config_file)):
@@ -50,9 +90,19 @@ def main(argv=None):
         exit()
 
     try:
-        deploy_to = conf[args.project][args.type]
+        conf_project = conf[args.project]
     except KeyError:
-        print('Missing section or option: deployment aborted')
+        print('Missing project "{}": deployment aborted'.format(args.project))
+        exit()
+
+    # if not already present, inject the package name into the configuration
+    if 'pkg_name' not in conf_project:
+        conf_project['pkg_name'] = args.project
+
+    try:
+        deploy_to = conf_project[args.type]
+    except KeyError:
+        print('Missing type "{}": deployment aborted'.format(args.type))
         exit()
 
     try:
